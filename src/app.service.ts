dcdea3bc7ec148afdc2f95bf78fd1fb5b268eb3d
NTPLSagarPatel <118171394+NTPLSagarPatel@users.noreply.github.com>
2024-02-28 16:51:57 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 @Injectable()
 export class AppService {
   getPersonName(name: string): string {
-    return `${name} <-- ('platformAPI')`;
+    return `${name} <--- ('platformAPI')`;
   }
 
   getHello(): string {
